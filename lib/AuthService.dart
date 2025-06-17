4ccc059ea4519b5d67a6a241c52de5271f69d5c3
RAHUL KUMAR <Rahul@kashpark.com>
2025-06-17 12:17:18 +0530

    First Push some

--- lib/AuthService.dart
+++ lib/AuthService.dart
@@ -2,48 +2,41 @@ import 'package:flutter/services.dart';
 import 'package:local_auth/local_auth.dart';
 import 'package:local_auth/error_codes.dart' as auth_error;
 
-class AuthService{
+class AuthService {
+  final LocalAuthentication _localAuth = LocalAuthentication();
 
-  final LocalAuthentication localAuth = LocalAuthentication();
+  Future<bool> authenticateLocally() async {
+    bool isAuthenticate = false;
 
+    bool isDeviceSupported = await _localAuth.isDeviceSupported();
+    bool canCheckBiometrics = await _localAuth.canCheckBiometrics;
 
-   Future<bool> authenticateLocally() async{
-     bool isAuthenticate = false;
+    if (!canCheckBiometrics || !isDeviceSupported) {
+      print("Biometric authentication not available.");
+      return false;
+    }
 
-     bool isDeviceSupported = await localAuth.isDeviceSupported();
-     bool canCheckBiometrics = await localAuth.canCheckBiometrics;
+    try {
+      isAuthenticate = await _localAuth.authenticate(
+          localizedReason: "We need to authenticate for user this app ",
+          options: AuthenticationOptions(
+              stickyAuth: true, useErrorDialogs: true, biometricOnly: false));
 
+      print("Auth Print :${isAuthenticate.toString()}");
+    } on PlatformException catch (e) {
+      if (e.code == auth_error.notEnrolled) {
+        // Add handling of no hardware here.
+      } else if (e.code == auth_error.lockedOut ||
+          e.code == auth_error.permanentlyLockedOut) {
+        // ...
+      } else {
+        // ...
+      }
+    } catch (e) {
+      isAuthenticate = false;
+      print("Error :$e");
+    }
 
-     if (!canCheckBiometrics || !isDeviceSupported) {
-       print("Biometric authentication not available.");
-       return false;
-     }
-
-
-     try{
-       isAuthenticate = await localAuth.authenticate(localizedReason: "We need to authenticate for user this app ",
-       options: AuthenticationOptions(
-         stickyAuth: true,
-         useErrorDialogs: true,
-         biometricOnly: false
-       ));
-
-       print("Auth Print :${isAuthenticate.toString()}");
-     }on PlatformException catch (e) {
-       if (e.code == auth_error.notEnrolled) {
-         // Add handling of no hardware here.
-       } else if (e.code == auth_error.lockedOut ||
-           e.code == auth_error.permanentlyLockedOut) {
-         // ...
-       } else {
-         // ...
-       }
-     }catch (e){
-       isAuthenticate = false;
-       print("Error :$e");
-     }
-
-
-     return isAuthenticate;
-   }
+    return isAuthenticate;
+  }
 }
